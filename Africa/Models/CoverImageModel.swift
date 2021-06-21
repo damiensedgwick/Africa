63088e7d8afe43cafa96da5a5ac27376fe1b1438
Damien Sedgwick <damiensedgwick@icloud.com>
2021-06-21 20:12:50 +0100

    Added cover image view and built decoder.

---
+++ Africa/Models/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Damien K. Sedgwick on 21/06/2021.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
