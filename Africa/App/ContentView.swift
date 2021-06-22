c1ddf49164b116a36b21b95b691fc844465420a6
Damien Sedgwick <damiensedgwick@icloud.com>
2021-06-22 19:25:32 +0100

    Added animal list item view

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -8,19 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
     var body: some View {
         NavigationView {
             List {
                 CoverImageView()
                   .frame(height: 300)
                   .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                
+                ForEach(animals) { animal in
+                    AnimalListItemView(animal: animal)
+                }
             }
+            .navigationBarTitle("Africa", displayMode: .large)
         }
-        .navigationBarTitle("Africa", displayMode: .large)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
     static var previews: some View {
         ContentView()
     }
