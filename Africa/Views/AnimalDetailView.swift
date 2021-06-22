6b20b6590350edbcdc45b02b92000dee1e57c0e7
Damien Sedgwick <damiensedgwick@icloud.com>
2021-06-22 20:58:08 +0100

    Completed detailed animal view.

---
+++ Africa/Views/AnimalDetailView.swift
@@ -0,0 +1,87 @@
+//
+//  AnimalDetailView.swift
+//  Africa
+//
+//  Created by Damien K. Sedgwick on 22/06/2021.
+//
+
+import SwiftUI
+
+struct AnimalDetailView: View {
+    
+    let animal: Animal
+    
+    var body: some View {
+        ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.primary)
+                    .background(
+                        Color.accentColor
+                            .frame(height: 6)
+                            .offset(y: 25)
+                    )
+                
+                Text(animal.headline)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.accentColor)
+                    .padding(.horizontal)
+                
+                Group {
+                    HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "In the Wild")
+                    InsetGalleryView(animal: animal)
+                }
+                .padding(.horizontal)
+                
+                Group {
+                    HeadingView(headingImage: "questionmark.circle", headingText: "Did you know?")
+                    InsetFactView(animal: animal)
+                }
+                .padding(.horizontal)
+                
+                Group {
+                    HeadingView(headingImage: "info.circle", headingText: "All about \(animal.name)")
+                    Text(animal.description)
+                        .multilineTextAlignment(.leading)
+                        .layoutPriority(1)
+                }
+                .padding(.horizontal)
+                
+                Group {
+                    HeadingView(headingImage: "map", headingText: "National Parks")
+                    InsetMapView()
+                }
+                .padding(.horizontal)
+                
+                Group {
+                    HeadingView(headingImage: "books.vertical", headingText: "Learn More")
+                    ExternalWeblinkView(animal: animal)
+                }
+                .padding(.horizontal)
+                    
+            }
+            .navigationBarTitle("Learn about \(animal.name)", displayMode: .inline)
+        }
+    }
+}
+
+struct AnimalDetailView_Previews: PreviewProvider {
+    
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        NavigationView {
+            AnimalDetailView(animal: animals[11])
+        }
+        .previewDevice("iPhone 12 Pro")
+    }
+}
