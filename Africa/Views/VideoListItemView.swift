7316a9a21ad02c80d2ae5be8fcb0d137fe8555dc
Damien Sedgwick <damiensedgwick@icloud.com>
2021-06-23 20:17:17 +0100

    Started implementing video list view.

---
+++ Africa/Views/VideoListItemView.swift
@@ -0,0 +1,54 @@
+//
+//  VideoListItem.swift
+//  Africa
+//
+//  Created by Damien K. Sedgwick on 23/06/2021.
+//
+
+import SwiftUI
+
+struct VideoListItemView: View {
+    
+    let video: Video
+    
+    var body: some View {
+        HStack(spacing: 10) {
+            ZStack {
+                Image(video.thumbnail)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 80)
+                    .cornerRadius(10)
+                
+                Image(systemName: "play.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 32)
+                    .shadow(radius: 10)
+            }
+            
+            VStack(alignment: .leading, spacing: 10, content: {
+                Text(video.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                
+                Text(video.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+            })
+        }
+    }
+}
+
+struct VideoListItem_Previews: PreviewProvider {
+    
+    static let videos: [Video] = Bundle.main.decode("videos.json")
+    
+    static var previews: some View {
+        VideoListItemView(video: videos[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
